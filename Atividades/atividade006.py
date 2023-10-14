3cd36c029a4b31fe0f46967d7ccb9c79b827b0bb
João Victor Santana <joaovsantana1249@gmail.com>
2023-10-14 15:55:42 -0300

    atividades

---
+++ Atividades/atividade006.py
@@ -0,0 +1,41 @@
+# copy, sorted, produtos.sort
+# Exercícios
+# Aumente os preços dos produtos a seguir em 10%
+# Gere novos_produtos por deep copy (cópia profunda)
+
+# Ordene os produtos por nome decrescente (do maior para menor)
+# Gere produtos_ordenados_por_nome por deep copy (cópia profunda)
+
+# Ordene os produtos por preco crescente (do menor para maior)
+# Gere produtos_ordenados_por_preco por deep copy (cópia profunda)
+
+
+from copy import deepcopy
+
+produtos = [
+    {'nome': 'Produto 5', 'preco': 10.00},
+    {'nome': 'Produto 1', 'preco': 22.32},
+    {'nome': 'Produto 3', 'preco': 10.11},
+    {'nome': 'Produto 2', 'preco': 105.87},
+    {'nome': 'Produto 4', 'preco': 69.90},
+]
+
+novos_produtos = deepcopy(produtos)
+
+novos_produtos = [{**p, 'preco': round(p['preco'] * 1.1, 2)} for p in novos_produtos]
+
+produtos_ordenados_por_nome = sorted(deepcopy(novos_produtos), key=lambda p: p['nome'], reverse=True)
+
+
+produtos_por_preco = sorted(deepcopy(novos_produtos), key=lambda p: p['preco'])
+
+
+
+
+print(*produtos, sep='\n')
+print()
+print(*novos_produtos, sep='\n')
+print()
+print(*produtos_ordenados_por_nome, sep='\n')
+print()
+print(*produtos_por_preco, sep='\n')
