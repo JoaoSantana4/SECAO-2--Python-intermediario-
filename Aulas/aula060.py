fb30e5858fe2b83cdd617778d798813d9a52e1f3
João Victor Santana <joaovsantana1249@gmail.com>
2023-10-25 21:37:46 -0300

    manipulando_arquivos

--- Aulas/aula060.py
+++ Aulas/aula060.py
@@ -26,6 +26,30 @@ caminho_arquivo += 'aula060.txt'
 # 
 #arquivo.close()
 
-with open(caminho_arquivo, 'w') as arquivo:
-    print('Hello world')
-    print('Arquivo vai ser fechado')
+nomes = ['Joao', 'Maria']
+
+with open(caminho_arquivo, 'w+') as arquivo:
+    arquivo.write('Linha 1\n')
+    arquivo.write('Linha 2\n')
+    arquivo.writelines(('Linha 3\n', 'Linha 4\n'))
+    arquivo.seek(0, 0)
+    print(arquivo.read())
+    print('Lendo')
+    arquivo.seek(0, 0)
+    print(arquivo.readline().strip()) #funciona como um next
+    print(arquivo.readline(), end= '') #readline está retornando uma str, então eu consigo usar funções para str
+    print(arquivo.readline().strip()) #.strip e end= '' são funções que removem os espaços, quebras de linhas são espaços
+    print(arquivo.readline().strip())
+
+    print('READLINES')
+    arquivo.seek(0, 0)
+    for linha in arquivo.readlines():
+        print(linha.strip())
+
+
+
+print('#' * 40)
+
+
+with open(caminho_arquivo, 'r') as arquivo:
+    print(arquivo.read())
